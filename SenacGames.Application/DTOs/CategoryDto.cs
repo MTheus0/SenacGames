0203ad93c1526071b856d1fb4915f8909b456c6b
Matheus <matverialmeida2@gmail.com>
2026-05-29 14:38:57 -0300

    Meu primeiro commit

---
+++ SenacGames.Application/DTOs/CategoryDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SenacGames.Application.DTOs
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Gamecount { get; set; }
+
+    }
+
+    public class CreateCategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+    }
+
+    public class UpdateCategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
